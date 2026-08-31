dc8c903b6112dada1fd04bb6f0ad3a985a7f320c
Renindstone <renzofarestenazoanunez@gmail.com>
2026-08-31 18:26:53 -0500

    InicializandoProyecto
    Cargando la estructura base del proyecto

---
+++ src/main/java/com/turismo/AsesorTuristicoMtcApplication.java
@@ -0,0 +1,13 @@
+package com.turismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsesorTuristicoMtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsesorTuristicoMtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turismo/AsesorTuristicoMtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsesorTuristicoMtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
